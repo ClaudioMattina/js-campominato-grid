58fe303bc1d4e8ac17bb3f55c891f56c9eb174ae
CLaudioBruni <claudio.bruni95@hotmail.com>
2022-03-04 19:54:10 +0100

    header e struttura finita

--- script.js
+++ script.js
@@ -1,7 +1,43 @@
 
 /* collego il main nel js */
-const mainBox = document.querySelector("main");
+const main = document.querySelector("main");
 
 /* collego nel js */
 const grid = document.getElementById("grid");
+
+/* ******************************************** FUNZIONIIIIIIII *********************************************** */
+
+/* funzione che restituisce un div con le classi crete da me nel css */
+const createSquare = () => {
+    const squareDiv = document.createElement("div");
+    squareDiv.classList.add("box");
+    return squareDiv;
+}
+
+/* **************************************************************************************************************** */
+
+
+
+
+
+
+
+
+
+
+
+for(i = 0; i < 100; i++){
+    /* creo una varialbile che crei un elemento tramite la funzione che ho creato */
+    const realSquare = createSquare();
+
+    /* aggiungo funzionalità al click con l'eventlistener */
+    /* che dice "al click aggiungi la classe clicked (classe che ho creato nel css)" */
+    realSquare.addEventListener("click" , function(){
+        realSquare.classList.toggle("clicked");
+    })
+
+    /* adesso inserisco lo square nel parent che volgio (in questo caso nel grid) */
+    grid.appendChild(realSquare);
+}
+
 
